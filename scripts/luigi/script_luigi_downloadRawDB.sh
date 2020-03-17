a740f2b9db9e437f1d0367b34010400b95131bbd
SCS <s.cadavid1587@hotmail.com>
2020-03-17 03:53:28 -0600

    script en bash para hacer descarga diaria en luigi

---
+++ scripts/luigi/script_luigi_downloadRawDB.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script for downloading data from 2010 to 2020 using Parameters
+# year, month, and day in downloadRawJSONData luigi task.
+
+for year in {2010..2019}
+do
+  for month in {1..12}
+  do
+    for day in {1..31}
+    do
+    PYTHONPATH="." luigi --module test2_311_data downloadRawJSONData --local-scheduler --year $year --month $month --day $day
+    sleep 2s
+    done
+  done
+done
