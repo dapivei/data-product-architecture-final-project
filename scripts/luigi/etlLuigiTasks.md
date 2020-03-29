857b71a3af817785379d8da285aef5ebabc0d18f
Daniela Pinto Veizaga <danielapintoveizaga@dapivei.local>
2020-03-29 09:56:58 -0400

    propuesta documentacin

---
+++ scripts/luigi/etlLuigiTasks.md
@@ -0,0 +1,45 @@
+<div class="tip" markdown="1">
+
+# ETL- *Luigi Tasks*
+<div align="justify">
+
+Los *Luigi Tasks* descritos a continuación ejecutan las siguientes tareas de manera consecutiva y dependiente:
+
+1) *downloadRawJSONData*: conexión a la API 311 NYC para extraer los datos y actualizaciones de la API en formato **.json**.
+
+2) :extracción y guardado (en RDS) de los metadatos importantes asociados al task *downloadRawJSONData*.
+
+3) *preprocParquetPandas*: preprocesamiento de los *.jsons* para convertirlos en formato *.parquet*.
+
+4) :extracción y guardado (en RDS) de los metadatos asociados al task *preprocParquetPandas*.
+
+
+## Ejecución de los *Tasks*
+
+**1)** Nos conecamos al **EC2** (máquina es para el procesamiento de la información y es donde viven las tareas que realiza luigi); para tener acceso a la misma tenemos que hacer un secure copy de la *llave.pem* al bastion:
+
+```
+ssh -i  /tu/ruta/llave-bastion.pem /tu/ruta/llave/ec2/llave.pem ubuntu@ip-ec2:/home/ubuntu/.ssh
+```
+
+
+**2)** Una vez dentro del EC2, se ejecuta:
+
+```
+PYTHONPATH='.' luigi --module luigi_tasks_S3 preprocParquetPandas --local-scheduler --year 2020 --month 3 --day 10 --bucket_name prueba-nyc311
+```
+
+En este ejemplo en particular, estamos pasando los parámetros: year `<2020>`, month `<3>`, day `<10>`, bucket_name `<prueba-nyc311>`. Los mismos deberán ser modificados según se requiera. Por default, el buck_name es prueba-nyc311.
+
+**Nota:** Como los tasks son dependendientes entre sí, basta con ejecutar el último tasks para que los tasks precedentes se ejecuten, en caso no haber sido ejecutados previamente.
+
+
+
+
+
+
+
+
+
+
+</div>
--- scripts/luigi/luigi_tasks_S3.py
+++ scripts/luigi/luigi_tasks_S3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Librerias necesarias para la ejecución de las tareas
 import boto3
 import json
 import luigi
@@ -18,65 +19,74 @@ path_raw = 's3://raw'
 path_preproc = 's3://preprocess'
 
 
-# path para guardar los datos
+# Path para guardar los datos
 class downloadRawJSONData(luigi.Task):
     '''
-    Descarga los datos de la API de 311 NYC en formato JSON en carpetas por
+    Tarea: Descarga los datos de la API de 311 NYC en formato JSON en carpetas por
     fecha con frecuencia diaria.
+
+    Input: Información de la API 311 NYC
+
+    Output: Documentos en formato .json ubicados en las carpetas en S3
+
+    Dependencias: Ninguna
     '''
-    # parametros:
+    # Parámetros requeridos al usuario:
     year = luigi.Parameter()
     month = luigi.Parameter()
     day = luigi.Parameter()
     bucket_name = luigi.Parameter(default="prueba-nyc311")
 
+    # Defining the path where the data will be saved.
     def output(self):
-        # Defining the loop for creating the variables:
+        # If the specified path isn't avaible, it will generate the path.
         output_path = f"{path_raw}/{self.year}/{self.month}/{self.day}/data_{self.year}_{self.month}_{self.day}.json"
         return luigi.contrib.s3.S3Target(path=output_path)
 
+    # Defining the bussiness logic of the task
     def run(self):
         # Autenticación en S3
         ses = boto3.session.Session(
             profile_name={self.bucket_name}, region_name='us-west-2')
         s3_resource = ses.resource('s3')
 
-        # Autenticación del cliente:
+        # Autenticación del cliente, ligado al documento "settings.toml":
         client = Socrata(settings.get('dburl'),
                          settings.get('apptoken'),
                          username=settings.get('user'),
                          password=settings.get('pass'))
 
-        # los resultados son retornados como un archivo JSON desde la API /
+        # Los resultados son retornados como un archivo JSON desde la API /
         # convertida a una lista de Python usando sodapy
         client.timeout = 1000
         limit = 1000000000
 
-        # crear carpeta raw
+        # Loop para generar carpetas/path donde serán guardados los datos:
+        # Crear carpeta raw
         if not os.path.exists(f'{path_raw}'):
             os.mkdir(f'{path_raw}')
         else:
             None
 
-        # crear carpeta year
+        # Crear carpeta year
         if not os.path.exists(f'{path_raw}/{self.year}'):
             os.mkdir(f'{path_raw}/{self.year}')
         else:
             None
 
-        # crear carpeta year/month
+        # Crear carpeta year/month
         if not os.path.exists(f'{path_raw}/{self.year}/{self.month}'):
             os.mkdir(f'{path_raw}/{self.year}/{self.month}')
         else:
             None
 
-        # crear carpeta  year/month/day
+        # Crear carpeta  year/month/day
         if not os.path.exists(f'{path_raw}/{self.year}/{self.month}/{self.day}'):
             os.mkdir(f'{path_raw}/{self.year}/{self.month}/{self.day}')
         else:
             None
 
-        # query
+        # Query to dump data retrieved in a .json file
         results = client.get(
             "erm2-nwe9", limit=limit, where=f"created_date between '{self.year}-{self.month}-{self.day}T00:00:00.000' and '{self.year}-{self.month}-{self.day}T23:59:59.999'")
         with self.output().open('w') as json_file:
@@ -85,53 +95,69 @@ class downloadRawJSONData(luigi.Task):
 
 class preprocParquetPandas(luigi.Task):
     '''
-    Convertir datos descargados en JSON a formato PARQUET.
+    Tarea: Convierte datos descargados en .JSON a formato .PARQUET., empleando
+    la librería pandas.
+
+    Input: documentos .json , previamente descargados en el task /
+    "downloadRawJSONData"
+
+    Output: documentos en formato .parquet ubicados en carpetas en S3
+
+    Dependencias: downloadRawJSONData
     '''
-    # parametros
+    # Parámetros requeridos al usuario
     year = luigi.Parameter()
     month = luigi.Parameter()
     day = luigi.Parameter()
 
+    # Definiendo dependecias con tasks previos
     def requires(self):
         return downloadRawJSONData(year=self.year, month=self.month, day=self.day)
 
+    # Defining where it writes the output
     def output(self):
         output_path = f"{path_preproc}/{self.year}/{self.month}/{self.day}/data_{self.year}_{self.month}_{self.day}.parquet"
         return luigi.contrib.s3.S3Target(path=output_path)
 
+    # Defining the bussiness logic of the task
     def run(self):
         # Autenticación en S3
         ses = boto3.session.Session(
             profile_name={self.bucket_name}, region_name='us-west-2')
         s3_resource = ses.resource('s3')
 
-        # crear carpeta preprocess
+        # Loop para generar carpetas/path preprocess, donde serán guardados los datos .parquet:
+
+        # Crear carpeta preprocess
         if not os.path.exists(f'{path_preproc}'):
             os.mkdir(f'{path_preproc}')
         else:
             None
 
-        # crear carpeta year
+        # Crear carpeta year
         if not os.path.exists(f'{path_preproc}/{self.year}'):
             os.mkdir(f'{path_preproc}/{self.year}')
         else:
             None
 
-        # crear carpeta year/month
+        # Crear carpeta year/month
         if not os.path.exists(f'{path_preproc}/{self.year}/{self.month}'):
             os.mkdir(f'{path_preproc}/{self.year}/{self.month}')
         else:
             None
 
-        # crear carpeta  year/month/day
+        # Crear carpeta  year/month/day
         if not os.path.exists(f'{path_preproc}/{self.year}/{self.month}/{self.day}'):
             os.mkdir(f'{path_preproc}/{self.year}/{self.month}/{self.day}')
         else:
             None
 
-        # convertir a parquet usando pandas
-        # lineas para correrlo sin y con requirements de downloadRawJSONData
+        # Convertir a .parquet usando la librería pandas
+
+        # Línea para correrlo sin requirements de downloadRawJSONData
         # df = pd.read_json(f"{path_raw}/{self.year}/{self.month}/{self.day}/data_{self.year}_{self.month}_{self.day}.json")
+        # Líneas para correrlo con requirements de downloadRawJSONData
+
         df = pd.read_json(self.input().path)
         # Solving problems of datatype: "nested column branch had multiple children"
         for col in df.columns:
@@ -142,65 +168,80 @@ class preprocParquetPandas(luigi.Task):
             if df[col].dtype == list:
                 df[col] = df[col].astype(str)
 
-        # guardar como parquet
+        # Guardar como parquet
         self.output().makedirs()
         df.to_parquet(self.output().path, engine='auto', compression='snappy')
 
 
 class preprocParquetSpark(luigi.Task):
     '''
-    Convertir datos descargados en JSON a formato PARQUET.
+    Tarea: Convierte datos descargados en .JSON a formato .PARQUET., empleando
+    la librería pyspark.
+
+    Input: documentos .json , previamente descargados en el task /
+    "downloadRawJSONData"
+
+    Output: documentos en formato .parquet ubicados en carpetas en S3
+
+    Dependencias: downloadRawJSONData
     '''
-    # parametros
+    # Parámetros
     year = luigi.Parameter()
     month = luigi.Parameter()
     day = luigi.Parameter()
 
+    # Definiendo dependencias con tasks previos
     def requires(self):
         return downloadRawJSONData(year=self.year, month=self.month, day=self.day)
 
+    # Defining where it writes the output
     def output(self):
         output_path = f"{path_preproc}/{self.year}/{self.month}/{self.day}/"
         return luigi.contrib.s3.S3Target(path=output_path)
 
+    # Defining the bussiness logic of the task
     def run(self):
         # Autenticación en S3
         ses = boto3.session.Session(
             profile_name={self.bucket_name}, region_name='us-west-2')
         s3_resource = ses.resource('s3')
+        # Loop para generar carpetas/path preprocess, donde serán guardados los datos .parquet:
 
-        # crear carpeta preprocess
+        # Crear carpeta preprocess
         if not os.path.exists(f'{path_preproc}'):
             os.mkdir(f'{path_preproc}')
         else:
             None
 
-        # crear carpeta year
+        # Crear carpeta year
         if not os.path.exists(f'{path_preproc}/{self.year}'):
             os.mkdir(f'{path_preproc}/{self.year}')
         else:
             None
 
-        # crear carpeta year/month
+        # Crear carpeta year/month
         if not os.path.exists(f'{path_preproc}/{self.year}/{self.month}'):
             os.mkdir(f'{path_preproc}/{self.year}/{self.month}')
         else:
             None
 
-        # crear carpeta  year/month/day
+        # Crear carpeta  year/month/day
         if not os.path.exists(f'{path_preproc}/{self.year}/{self.month}/{self.day}'):
             os.mkdir(f'{path_preproc}/{self.year}/{self.month}/{self.day}')
         else:
             None
 
-        # convertir a parquet usando pyspark:
-        # crear sesión en spark
+        # Convertir a parquet usando pyspark:
+        # Crear sesión en spark
         sc = SparkContext.getOrCreate()
         sqlContext = SQLContext(sc)
-        # lineas para correrlo sin y con requirements de downloadRawJSONData
+
+        # Lineas para correrlo sin y con requirements de downloadRawJSONData
         # df = sqlContext.read.json(f"{path_raw}/{self.year}/{self.month}/{self.day}/data_{self.year}_{self.month}_{self.day}.json")
+
+        # Linea para correrlo con requirements de downloadRawJSONData
         df = sqlContext.read.json(self.input().path)
 
-        # guardar como parquet
+        # Guardar como parquet
         self.output().makedirs()
         df.write.parquet(self.output().path, mode="overwrite")
