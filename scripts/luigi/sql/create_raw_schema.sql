b3628b98f239c633acbe31cd9a0571afc56f9932
Diego Villa <villa.lizarraga@gmail.com>
2020-03-30 12:58:05 -0600

    actualizacion table names

--- scripts/luigi/sql/create_raw_schema.sql
+++ scripts/luigi/sql/create_raw_schema.sql
@@ -1,8 +1,8 @@
 create schema if not exists raw;
 
-drop table if exists raw.etl_ejecucion;
+drop table if exists raw.etl_execution;
 
-create table raw.etl_ejecucion (
+create table raw.etl_execution (
   "name" TEXT,
   "extention" TEXT,
   "schema" TEXT,
@@ -20,4 +20,4 @@ create table raw.etl_ejecucion (
   "status" TEXT
 );
 
-comment on table raw.etl_ejecucion is 'Metadatos de ejecucion del etl RAW';
+comment on table raw.etl_ejecucion is 'Metadata from ETL-RAW';
