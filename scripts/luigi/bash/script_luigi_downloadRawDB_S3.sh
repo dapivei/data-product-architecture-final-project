9375336ef44d483301c26118032b950d177692ba
Sebastián Cadavid-Sánchez <11048067+C1587S@users.noreply.github.com>
2020-04-24 20:55:17 -0500

    descarga de datos y meta/  (no usar)solo 2 tareas

--- scripts/luigi/bash/script_luigi_downloadRawDB_S3.sh
+++ scripts/luigi/bash/script_luigi_downloadRawDB_S3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script for downloading data from 2010 to 2020 using Parameters
+# Script for preprocessing data from 2010/1 to 2020/3 using Parameters
 # year, month, and day in downloadRawJSONData luigi task.
 
 for year in {2010..2019}
@@ -9,8 +9,20 @@ do
   do
     for day in {1..31}
     do
-    PYTHONPATH="." luigi --module luigi_local_downloadRaw_S3 downloadRawJSONData --local-scheduler --year $year --month $month --day $day --bucket_name 'luigi_dpa'
-    sleep 2s
+    PYTHONPATH="." PYTHONPATH="." AWS_PROFILE=luigi_dpa  luigi --module luigi_orchestration_ec2 Task_20_metaDownload --local-scheduler --year $year --month $month --day $day
+    sleep 0.5s
     done
   done
 done
+
+# para 2020
+for month in {1..3}
+  do
+    for day in {1..31}
+      do
+    PYTHONPATH="." PYTHONPATH="." AWS_PROFILE=luigi_dpa  luigi --module luigi_orchestration_ec2 Task_20_metaDownload --local-scheduler --year 2020 --month $month --day $day
+    sleep 0.5s
+    
+    done
+done
+
