e44746bfd056bb0cc3f39b0fb58f0ba2d3b3d8f7
Pilo1961 <jphmusi@gmail.com>
2020-04-25 18:08:07 +0000

    genera schemas de rds

---
+++ scripts/luigi/rds_setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import psycopg2 as ps
+from dynaconf import settings
+
+#connect
+conn=ps.connect(host=settings.get('host'),
+                port=settings.get('port'),
+                database=settings.get('database'),
+                user=settings.get('usr'),
+                password=settings.get('password'))
+cur = conn.cursor()
+
+#read file
+f=open('sql/create_raw_schema.sql','r')
+sql=str(f.read())
+cur.execute(sql)
+conn.commit()
+
+f=open('sql/create_preprocessed_schema.sql','r')
+sql=str(f.read())
+cur.execute(sql)
+conn.commit()
+
+cur.close()
+conn.close()
