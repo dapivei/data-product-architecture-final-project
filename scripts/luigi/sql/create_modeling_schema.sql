baa54d732551505783f5b76f2ae65cdca96e3bcc
Pilo1961 <jphmusi@gmail.com>
2020-04-28 22:52:06 +0000

    esquema metadata modelling

--- scripts/luigi/sql/create_modeling_schema.sql
+++ scripts/luigi/sql/create_modeling_schema.sql
@@ -5,12 +5,18 @@ drop table if exists modeling.ejecucion;
 create table modeling.ejecucion (
   "model_name" TEXT,
   "model_type" TEXT,
+  "schema" TEXT,
+  "action" TEXT,
+  "creator" TEXT,
+  "machine" TEXT,
+  "ip" TEXT,
+  "date" TEXT,
+  "location" TEXT,
+  "status" TEXT,
   "max_depth" TEXT,
   "criterion" TEXT,
   "n_estimators" TEXT,
-  "score_train" TEXT,
-  "score_validation" TEXT
-);
+  "score_train" TEXT);
 
-comment on table modeling.etl_ejecucion is 'Metadatos de ejecucion del
+comment on table modeling.ejecucion is 'Metadatos de ejecucion del
 etl MODELING';
