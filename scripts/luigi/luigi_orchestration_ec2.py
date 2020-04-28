728053ef78decd9185d059468a4ab141a3fe7713
Sebastián Cadavid-Sánchez <11048067+C1587S@users.noreply.github.com>
2020-04-28 04:59:15 -0500

    append de mlpreproc falta ajustar loop

--- scripts/luigi/luigi_orchestration_ec2.py
+++ scripts/luigi/luigi_orchestration_ec2.py
@@ -517,17 +517,15 @@ class Task_70_mlPreproc(luigi.Task):
     year = luigi.Parameter()
     month = luigi.Parameter()
     day = luigi.Parameter()
+    parte = luigi.Parameter()
     # ==============================
     def requires(self):
-        return Task_71_mlPreproc_firstTime(year=self.year, month=self.month, day=self.day)
+        return Task_60_metaClean(year=self.year, month=self.month, day=self.day)
 
-    def output(self):
-        # guarda los datos en s3://prueba-nyc311/raw/.3..
-        output_path = f"s3://{self.bucket}/mlpreproc/mlPreproc.parquet"
-        return luigi.contrib.s3.S3Target(path=output_path)
-
-
-    def run(self):
+    def input(self):
+        '''
+        Acá se lee el dataframe input diario
+        '''
         import io
         # Autenticación en S3
         ses = boto3.session.Session(
@@ -535,40 +533,81 @@ class Task_70_mlPreproc(luigi.Task):
         buffer=io.BytesIO()
         s3_resource = ses.resource('s3')
         obj = s3_resource.Bucket(name=self.bucket)
-
         #lectura de datos
+        #key = f"mlpreproc/mlPreproc_until_part{part_num-1}.parquet"
         key = f"cleaned/{self.year}/{self.month}/{self.day}/data_{self.year}_{self.month}_{self.day}.parquet"
         parquet_object = s3_resource.Object(bucket_name=self.bucket, key=key) # objeto
         data_parquet_object = io.BytesIO(parquet_object.get()['Body'].read())
         df = pd.read_parquet(data_parquet_object)
-
-        # Filtramos los casos de ruido para la agencia NYPD
-        print(df.shape)
         df=df.loc[(df["agency"]=='nypd') & (df["complaint_type"].str.contains("noise")),:]
         df=df.reset_index(drop=True)
+        return df
 
+    def output(self):
+        # guarda los datos en s3://prueba-nyc311/mlpreproc/...
+        part_num = int(self.parte)
+        output_path = f"s3://{self.bucket}/mlpreproc/mlPreproc_until_part{part_num}.parquet"
+        # output_path = f"s3://{self.bucket}/mlpreproc/mlPreproc_until_part1.parquet"
+        return luigi.contrib.s3.S3Target(path=output_path)
+
+    def run(self):
+        import datetime
+        year_num = int(self.year)
+        month_num = int(self.month)
+        day_num = int(self.month)
+        date = datetime.datetime(year_num, month_num, day_num)
+        day_of_year = (date - datetime.datetime(year_num, 1, 1)).days + 1
+
+        print(day_of_year)
         #cuenta los registros y colapsa el df
-        #df['counts']=1
-        #df=df.loc[:,['created_date','counts']]
-        #df=df.groupby(['created_date']).count()
+        df = self.input()
+        df['counts']=1
+        df=df.loc[:,['created_date','counts']]
+        df=df.groupby(['created_date']).count()
+        print(df.head())
 
-        key = f"s3://{self.bucket}/mlpreproc/mlPreproc.parquet"
-        parquet_object = s3_resource.Object(bucket_name=self.bucket, key=key) # objeto
+        # Autenticación en S3
+        import io
+        ses = boto3.session.Session(
+            profile_name='luigi_dpa', region_name='us-west-2')
+        buffer=io.BytesIO()
+        s3_resource = ses.resource('s3')
+        obj = s3_resource.Bucket(name=self.bucket)
+        # acá leemos los
+        part_num = int(self.parte)
+        key = f"mlpreproc/mlPreproc_until_part{part_num-1}.parquet"
+        parquet_object = s3_resource.Object(bucket_name=self.bucket, key=key)
         data_parquet_object = io.BytesIO(parquet_object.get()['Body'].read())
         df2 = pd.read_parquet(data_parquet_object)
 
-        df2=df2.append(df)
-        df2.drop_duplicates(inplace=True)
-        print(df2)
-        #pasa a formato parquet
-        df2.to_parquet(self.output().path, engine='auto', compression='snappy')
+        print("="*50)
+        print("segundo dataframe ingresado")
+        print(day_of_year)
+        print(df.head())
+        print(df.shape)
+        print(df2.head())
+        print(df2.shape)
+        print("="*50)
+
+        # append los dataframes
+        joined_df=df2.append(df)
+        joined_df.drop_duplicates(inplace=True)
+
+        print("*"*50)
+        print("*"*50)
+        print("*** Appended dataframe ***")
+        print(joined_df.head())
+        print("*"*50)
+        print("*"*50)
+        # #pasa a formato parquet
+        joined_df.to_parquet(self.output().path, engine='auto', compression='snappy')
+        #df.to_parquet(self.output().path, engine='auto', compression='snappy')
 
         #table = pa.Table.from_pandas(df)
         #s3 = fs.S3FileSystem(region='us-west-2')
         # Write direct to your parquet file
         #output_path = f"s3://{self.bucket}/mlpreproc/mlPreproc.parquet"
         #pq.write_to_dataset(table , root_path=output_path,filesystem=s3)
-
 
 
 class Task_71_mlPreproc_firstTime(luigi.Task):
@@ -654,9 +693,73 @@ class Task_71_mlPreproc_firstTime(luigi.Task):
 
 
 
-'''
-falta class 80 mlPreproc metadata
-'''
+class Task_80_metaMlPreproc(luigi.task.WrapperTask):
+    '''
+    Guardar los metadatos de mlPreproc
+    '''
+    # ==============================
+    # parametros:
+    # ==============================
+    bucket = luigi.Parameter(default="prueba-nyc311")
+    year = luigi.Parameter()
+    month = luigi.Parameter()
+    day = luigi.Parameter()
+    # ==============================
+
+    def requires(self):
+        return Task_71_mlPreproc_firstTime(year=self.year, month=self.month, day=self.day)
+
+    def run(self):
+        # se instancia la clase raw_metadata()
+        cwd = os.getcwd()  # directorio actual
+        raw_meta = mlPreproc_metadata()
+        raw_meta.name = f"data_{self.year}_{self.month}_{self.day}"
+        raw_meta.user = str(getpass.getuser())
+        raw_meta.machine = str(platform.platform())
+        raw_meta.ip = execv("curl ipecho.net/plain ; echo", cwd)
+        raw_meta.creation_date = str(datetime.datetime.now())
+        raw_meta.location = f"{path_raw}/{raw_meta.name}"
+        raw_meta.param_year = str(self.year)
+        raw_meta.param_month = str(self.month)
+        raw_meta.param_day = str(self.day)
+        raw_meta.param_bucket = str(self.bucket)
+
+        ubicacion_completa = f"{raw_meta.location}.json"
+        meta = raw_meta.info()  # extrae info de la clase
+
+        print("=" * 100)
+        print(meta)
+        print("complete name: ", ubicacion_completa)
+        print("name: ", raw_meta.name)
+        print("extensión: ", raw_meta.extention)
+        print("schema: ", raw_meta.schema)
+        print("tamaño: ", raw_meta.size)
+        print("action: ", raw_meta.action)
+        print("usuario: ", raw_meta.user)
+        print("maquina: ", raw_meta.machine)
+        print("ip: ", raw_meta.ip)
+        print("fecha de creación: ", raw_meta.creation_date)
+        print("ubicación: ", raw_meta.location)
+        print("param [year]: ", raw_meta.param_year)
+        print("param [month]: ", raw_meta.param_month)
+        print("param [day]: ", raw_meta.param_day)
+        print("param [bucket]: ", raw_meta.param_bucket)
+        print("=" * 100)
+
+        # conectarse a la base de datos y guardar a esquema raw.etl_execution
+        conn = ps.connect(host=settings.get('host'),
+                          port=settings.get('port'),
+                          database="nyc311_metadata",
+                          user=settings.get('usr'),
+                          password=settings.get('password'))
+        cur = conn.cursor()
+        columns = "(name, extention, schema, action, creator, machine, ip, creation_date, size, location,status, param_year, param_month, param_day, param_bucket)"
+        sql = "INSERT INTO mlpreproc.feature_engineering" + columns + \
+            " VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
+        cur.execute(sql, meta)
+        conn.commit()
+        cur.close()
+        conn.close()
 
 class Task_91_ml_firstTime(luigi.Task):
     '''
