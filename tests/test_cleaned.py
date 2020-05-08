8767802f63d3eeea1506d64af5482baf686c6d3b
Diego Villa <villa.lizarraga@gmail.com>
2020-05-07 19:21:44 -0500

    Se adapto el codigo a marbles, se corrigieron los errores de las fuciones setUp y tearDown y se actualizaron las notas y mensajes

--- tests/test_cleaned.py
+++ tests/test_cleaned.py
@@ -2,38 +2,43 @@
 # Unittest for cleaned data
 #=====================================
 
-import unittest
+import marbles.core
 import pandas as pd
 import datetime
 from datetime import date
 
+
 # load data from path_cleaned = 's3://prueba-nyc311/cleaned'
-df = pd.read_json("/Users/danielapintoveizaga/GitHub/data-product-architecture-final-project/scripts/luigi/raw/2013/10/6/data_2013_10_6.json")
+file = '/Users/diegovillalizarraga/Maestria Ciencia de Datos ITAM/2do Semestre/Metodos de gran escala/Repositorio/data-product-architecture-final-project/tests/clean_2015_4_14.parquet'
+df = pd.read_parquet(file)
 
-#df = pd.read_parquet("/Users/danielapintoveizaga/Downloads/data_2011_10_10.parquet") # corregir path
 
-class DateTestCase(unittest.TestCase): #inheriting from unittest.TestCase
+class DateTestCase(marbles.core.TestCase):
 
-#    def setUp(self):        # hay que corregir esta parte no está funcionado
-#        self.df = df        # load data from path_cleaned = 's3://prueba-nyc311/cleaned'
+    def setUp(self):
+        self.df = df
 
-#    def tearDown(self):
-#        delattr(self, 'df') # taking the data of my test case
-                            # \to avoid pollution
+    def tearDown(self):
+        delattr(self, 'df')
 
     def test_for_closed_date_greater_than_created_date(self):
         '''
 
-        Función para evaluar si la closed_date es igual o mayor a created_date
+        Función para evaluar si la closed_date es mayor a created_date
 
         '''
 
-        valid = df['closed_date'] >= df['created_date']
-        self.assertTrue(all(valid), note = "Si la prueba falla, puede deberse a que la columna closed_date tiene valor nulo.")
+        _filtro = self.df['closed_date'] < self.df['created_date']
+        msg = f'Se detectaron {self.df[_filtro].shape[0]} casos en los que la closed_date sucede antes que la created_date.'
+        note = 'Es imposible que un issue se cierre antes de ser abierto, actualmente existen casos en los que esta sucediendo esto.'
+
+        if df['closed_date'] is not None:
+            _valid = self.df['closed_date'] >= self.df['created_date']
+            self.assertTrue(all(_valid), msg=msg, note=note)
+
 
 
     def test_for_years_out_of_range(self):
-
         '''
 
         Evaluando que los valores de created_date y closed_date están entre 2010 y
@@ -41,39 +46,13 @@ class DateTestCase(unittest.TestCase): #inheriting from unittest.TestCase
 
         '''
 
-        minimun = datetime.date(2010,1,1)
+        minimum = datetime.date(2010,1,1)
         today = date.today()
-        self.assertGreaterEqual(min(df['created_date']), minimun, note= "Si la prueba falla, puede deberse a que los datos fueron actualizados y se incluyeron registros que previamente no existían.")
-        self.assertLessEqual(max(df['created_date']), today)
-        self.assertGreaterEqual(min(df['closed_date']), minimun, note= "Si la prueba falla, puede deberse a que los datos fueron actualizados y se incluyeron registros que previamente no existían.")
-        self.assertLessEqual(max(df['closed_date']), today)
+        self.assertGreaterEqual(min(self.df['created_date']), minimum, note='La fuente de donde se obtuvieron los datos se especifica que son registros del 2010 en adelante por lo que no tendria sentido tener registros con created_date previos al 2010.')
+        self.assertLessEqual(max(self.df['created_date']), today, note='No tendria sentido que hubiera una created_date posterior a la fecha del dia de hoy.')
+        self.assertGreaterEqual(min(self.df['closed_date']), minimum, note='La fuente de donde se obtuvieron los datos se especifica que son registros del 2010 en adelante por lo que no tendria sentido tener registros con closed_date previos al 2010.')
+        self.assertLessEqual(max(self.df['closed_date']), today, note='No tendria sentido que hubiera una close_date posterior a la fecha del dia de hoy.')
 
 
 if __name__ == '__main__':
-    unittest.main()
-
-
-#import marbles.core
-
-#class LowerTestCase(marbles.core.TestCase):
-
-#    def test_lower_case(self):
-#        cols = df.columns
-#        for col in cols:
-#            try:
-#                self.assertEqual(df[col], df[col].str.lower(), note= "En el guardado 'cleaned', para tener datos homegeneos, convertimos todos los valores strings a minúsculas")
-#            except:
-#                next
-
-        #self.assertEqual(df.upper(), 'FOO', note=" the names should be uppercase because bla bla bla")
-
-    #def test_isupper_w_marbles(self):
-    #    self.assertTrue('foo'.isupper())
-    #    self.assertFalse('Foo'.isupper())
-
-    #def test_split_w_marbles(self):
-    #    s = 'hello world'
-    #    self.assertEqual(s.split(), ['hello', 'world'])
-#        # check that s.split fails when the separator is not a string
-#        with self.assertRaises(TypeError):
-#            s.split(2)
+    marbles.core.main()
