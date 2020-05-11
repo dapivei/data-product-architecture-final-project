211e600c9b19faa46c4dcccf407a1b28abbd22bf
Diego Villa <villa.lizarraga@gmail.com>
2020-05-11 18:19:03 -0500

    Se agrega comando para generar output en pruebas de marbles

--- scripts/luigi/unit_test/run_clean_test.sh
+++ scripts/luigi/unit_test/run_clean_test.sh
@@ -10,6 +10,8 @@ P_DAY=$day P_MONTH=$month P_YEAR=$year P_BUCKET=$bucket python3 -m marbles unit_
 #asi redireige STDERR a un archivo pero no imprime en pantalla
 #P_DAY=$day P_MONTH=$month P_YEAR=$year P_BUCKET=$bucket python3 -m marbles test_cleaned.py &> clean_test_output.txt
 
+#python3 -m marbles test_cleaned.py 2>&1 | tee cleaned_output.txt
+
 #NOTA: si redirigo el STDERR al archivo no imprime nada de la pruba en el output de luigi
 # y si llamo cat para imprimir ya no truena nunca (por que se sobreescribe el ultimo STDERR)
 # entonces hay que buscar como darle la vuelta a eso o no escribir info sobre la prueba en matadata
