2c31455d7a7dfeaade2f8fd1f1ed34782fcb85c7
Pilo1961 <jphmusi@gmail.com>
2020-05-12 16:15:49 +0000

    prueba unitaria feature funcionando. Add mock para que escriba en s3

--- scripts/luigi/unit_test/run_feature_test.sh
+++ scripts/luigi/unit_test/run_feature_test.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-day=${1}
-month=${2}
-year=${3}
-bucket=${4}
+bucket=${1}
 
 #asi redireige STDERR a un archivo e imprime en pantalla
-P_DAY=$day P_MONTH=$month P_YEAR=$year P_BUCKET=$bucket python3 -m marbles unit_test/test_cleaned.py 2>&1 | tee clean_test_output.txt
+P_BUCKET=$bucket python3 -m marbles unit_test/test_feature.py 2>&1 | tee feature_test_output.txt
 
 #si encuentra FAIL regresa estatus fallido.
 #De otro modo regresa estatus completo
-! cat clean_test_output.txt | grep FAIL
-
+! cat feature_test_output.txt | grep FAIL
--- scripts/luigi/unit_test/test_feature.py
+++ scripts/luigi/unit_test/test_feature.py
@@ -7,17 +7,35 @@ import pandas as pd
 import datetime
 import sys
 
-# load data from path_ml = s3://prueba-nyc311/ml'
-file = '/Users/diegovillalizarraga/Maestria Ciencia de Datos ITAM/2do Semestre/Metodos de gran escala/Repositorio/data-product-architecture-final-project/tests/ml.parquet'
-df = pd.read_parquet(file)
+import os
+import io
+import boto3
+import botocore
+import getpass
+import warnings
+
 
 # Para las entrega ponemos algun valor de una columna que actualmente es 1 en 0 para hacer que el test falle
 #df['y_2010'].iloc[2] = 0
 #df['m_1'].iloc[2] = 5
 
 class OneHotTestCase(marbles.core.TestCase):
+    BUCKET="prueba-nyc311"
 
     def setUp(self):
+        OneHotTestCase.BUCKET = os.environ.get('P_BUCKET', OneHotTestCase.BUCKET)
+
+        ses = boto3.session.Session(profile_name='luigi_dpa', region_name='us-west-2')
+        buffer=io.BytesIO()
+        s3_resource = ses.resource('s3')
+        obj = s3_resource.Bucket(name=OneHotTestCase.BUCKET)
+
+        #lectura de datos
+        key = f"ml/ml.parquet"
+        parquet_object = s3_resource.Object(bucket_name=OneHotTestCase.BUCKET, key=key) # objeto
+        data_parquet_object = io.BytesIO(parquet_object.get()['Body'].read())#.decode() # info del objeto
+        df = pd.read_parquet(data_parquet_object)
+
         self.df = df
 
     def tearDown(self):
@@ -25,15 +43,12 @@ class OneHotTestCase(marbles.core.TestCase):
 
     def test_created_date_year_vs_onehot(self):
         '''
-
         Función para evaluar si es consistente el año del created_date (year) vs el
         one hot y_{year} == 1
-
         '''
-
         for row in range(0, self.df.shape[0]):
-            _year = self.df['created_date'].iloc[row].year
-            _valid = self.df[f'y_{_year}'].iloc[row]
+            _year = self.df['created_date'].iloc[row].year-2010
+            _valid = self.df[f'created_date_year_{_year}'].iloc[row]
             self.assertEqual(_valid, 1,
             msg = f'La fila {row} para la columna y_{_year} tiene un 0 cuando debería de tener 1.',
             note = 'Si sacamos el año de la fecha created date (year) debería de estar como True (1) la bandera en la columna de año correspondiente con formato onehot para garantizar consistencia.')
@@ -46,8 +61,8 @@ class OneHotTestCase(marbles.core.TestCase):
         '''
 
         for row in range(0, self.df.shape[0]):
-            _month = self.df['created_date'].iloc[row].month
-            _valid = self.df[f'm_{_month}'].iloc[row]
+            _month = self.df['created_date'].iloc[row].month-1
+            _valid = self.df[f'created_date_month_{_month}'].iloc[row]
             self.assertEqual(_valid, 1,
             msg = f'La fila {row} para la columna m_{_month} tiene un 0 cuando debería de tener 1.',
             note = 'Si sacamos el mes de la fecha created date (year) debería de estar como True (1) la bandera en la columna de mes correspondiente con formato onehot para garantizar consistencia.')
