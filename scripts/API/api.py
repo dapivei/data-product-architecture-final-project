38a28eb7a787720708c4585d428aed35078d7493
Diego Villa <villa.lizarraga@gmail.com>
2020-06-02 01:34:55 -0500

    Se agrega endpoint de prección

--- scripts/API/api.py
+++ scripts/API/api.py
@@ -13,7 +13,7 @@ ns = api.namespace('API-NYC311',
 
 
 
-@ns.route('/info_modelo', endpoint='endpoint_info_modelo')
+@ns.route('/model_info', endpoint='endpoint_model_info')
 class Modelo(Resource):
     """Aquí debe ir la descripción de la clase Modelo
     Aquí debe ir el segundo renglón
@@ -21,7 +21,7 @@ class Modelo(Resource):
     """
     parser = ns.parser()
     parser.add_argument('anio', required=True, type=int, help='Año de la predicción', nullable=True)
-    parser.add_argument('mes', required=True, type=int, help='Mes de la predicción', choices=(1,2,3,4), default=1, nullable=True)
+    parser.add_argument('mes', required=True, type=int, help='Mes de la predicción', choices=(1,2,3,4,5,6,7,8,9,10,11,12), default=1, nullable=True)
     parser.add_argument('dia', required=True, type=int, help='Día de la predicción', nullable=True)
 
     @ns.expect(parser, validate=True)
@@ -38,30 +38,45 @@ class Modelo(Resource):
         cur = conn.cursor()
         sql = f"SELECT * FROM modeling.ejecucion WHERE param_year='{anio}' and param_month='{mes}' and param_day='{dia}';"
         cur.execute(sql)
-        data=cur.fetchall()
+        data = cur.fetchall()
         conn.commit()
         cur.close()
         conn.close()
-        return  data
+        return data
 
 
-@ns.route("/predict")
+@ns.route('/prediction', endpoint='endpoint_prediction')
 class Predict(Resource):
-
-    #obj_Rita = Rita()
-    #meses = obj_Rita.ObtenerMeses()
-
+    """Aquí debe ir la descripción de la clase Modelo
+    Aquí debe ir el segundo renglón
+    Y aquí debe ir el tercer renglón
+    """
     parser = ns.parser()
     parser.add_argument('anio', required=True, type=int, help='Año de la predicción', nullable=True)
-    parser.add_argument('mes', required=True, type=int, help='Mes de la predicción', choices=(1,2,3,4), default=1, nullable=True)
+    parser.add_argument('mes', required=True, type=int, help='Mes de la predicción', choices=(1,2,3,4,5,6,7,8,9,10,11,12), default=1, nullable=True)
     parser.add_argument('dia', required=True, type=int, help='Día de la predicción', nullable=True)
+
 
     @ns.expect(parser, validate=False)
     def get(self):
         anio = request.args.get('anio')
         mes = request.args.get('mes')
         dia = request.args.get('dia')
-        return {'Anio': anio, 'mes': mes, 'dia':dia}
+
+        conn = ps.connect(host=settings.get('host'),
+                          port=settings.get('port'),
+                          database="nyc311_metadata",
+                          user=settings.get('usr'),
+                          password=settings.get('password'))
+        cur = conn.cursor()
+        sql = f"SELECT * FROM prediction.ejecucion WHERE pred_date='{anio}-{mes}-{dia}';"
+        cur.execute(sql)
+        data = cur.fetchall()
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return data
 
 
 if __name__ == '__main__':
